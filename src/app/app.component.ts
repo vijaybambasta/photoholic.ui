bed6542f61b03b7fa0e65d51c261d92dc157653f
vijaybambasta <99092446+vijaybambasta@users.noreply.github.com>
2022-02-09 13:36:03 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -123,7 +123,7 @@ doValiation(){
     const apiURL = new Settings().apiURL;
     
     
-    this.http.post('https://9orq9rf037.execute-api.us-west-1.amazonaws.com/dev', formData)
+    this.http.post('https://0vr72fw1g9.execute-api.us-west-1.amazonaws.com/dev', formData)
       .subscribe(res => {
         console.log(res);
         alert('Uploaded Successfully.');
