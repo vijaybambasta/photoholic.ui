9e4a599be3d6f8ad3eeedc22abf68f64d2876e32
vijaybambasta <99092446+vijaybambasta@users.noreply.github.com>
2022-02-09 14:07:38 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -123,7 +123,7 @@ doValiation(){
     const apiURL = new Settings().apiURL;
     
     
-    this.http.post('https://0vr72fw1g9.execute-api.us-west-1.amazonaws.com/dev', formData)
+    this.http.post('https://api.photoholic.live', formData)
       .subscribe(res => {
         console.log(res);
         alert('Uploaded Successfully.');
